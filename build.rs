f5960103f5698b97595d53534d281bb7f286ccf0
randymcmillan <randymcmillan@protonmail.com>
2026-03-20 19:36:48 -0400

    build.rs:target_os config

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
 fn main() {
-    // Link against Apple's Accelerate framework for BLAS (cblas_sgemv, cblas_sger)
-    println!("cargo:rustc-link-lib=framework=Accelerate");
+    #[cfg(target_os = "macos")]
+    {
+        // Link against Apple's Accelerate framework for BLAS (cblas_sgemv, cblas_sger)
+        println!("cargo:rustc-link-lib=framework=Accelerate");
+    }
+    #[cfg(not(target_os = "macos"))]
+    {
+    }
 }
