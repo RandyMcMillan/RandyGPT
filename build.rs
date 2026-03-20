45cfdaebe7f0ef23a1bec3b88256c90602d69256
randymcmillan <randymcmillan@protonmail.com>
2026-03-20 18:26:04 -0400

    chore(build): Ensure main function is always present in build script

--- build.rs
+++ build.rs
@@ -1,5 +1,10 @@
-#[cfg(target_os = "macos")]
 fn main() {
-    // Link against Apple's Accelerate framework for BLAS (cblas_sgemv, cblas_sger)
-    println!("cargo:rustc-link-lib=framework=Accelerate");
+    #[cfg(target_os = "macos")]
+    {
+        // Link against Apple's Accelerate framework for BLAS (cblas_sgemv, cblas_sger)
+        println!("cargo:rustc-link-lib=framework=Accelerate");
+    }
+    #[cfg(not(target_os = "macos"))]
+    {
+    }
 }
