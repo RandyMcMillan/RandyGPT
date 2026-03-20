ec91689dfddcb1776b9fb085c5caebbeea09434f
randymcmillan <randymcmillan@protonmail.com>
2026-03-20 18:23:35 -0400

    chore(build): Conditionally link Accelerate framework

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+#[cfg(target_os = "macos")]
 fn main() {
     // Link against Apple's Accelerate framework for BLAS (cblas_sgemv, cblas_sger)
     println!("cargo:rustc-link-lib=framework=Accelerate");
