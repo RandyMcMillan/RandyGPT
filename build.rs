3ddac147a2e6176c8934806fd0318671baaf2e31
randymcmillan <randymcmillan@protonmail.com>
2026-03-21 11:46:53 -0400

    build.rs

--- build.rs
+++ build.rs
@@ -4,17 +4,23 @@ fn main() {
         // For macOS ARM (Apple Silicon)
         #[cfg(target_arch = "aarch64")]
         {
-            println!("cargo:rustc-link-lib=framework=Accelerate");
+            // println!("cargo:rustc-link-lib=framework=Accelerate");
         }
         // For macOS Intel
         #[cfg(target_arch = "x86_64")]
         {
-            println!("cargo:rustc-link-lib=framework=Accelerate");
+            // println!("cargo:rustc-link-lib=framework=Accelerate");
         }
     }
     #[cfg(not(target_os = "macos"))]
     {
-        // Explicitly link MKL runtime library for non-macOS targets (e.g., Linux with MKL feature)
-        println!("cargo:rustc-link-lib=mkl_rt");
+        #[cfg(target_arch = "aarch64")]
+        {
+            // println!("cargo:rustc-link-lib=mkl_rt");
+        }
+        #[cfg(target_arch = "x86_64")]
+        {
+            // println!("cargo:rustc-link-lib=mkl_rt");
+        }
     }
 }
