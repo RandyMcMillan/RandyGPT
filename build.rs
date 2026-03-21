616b9d6aa12cfc6de37b85282a6e3d94e886d184
randymcmillan <randymcmillan@protonmail.com>
2026-03-21 17:12:03 -0400

    build.rs

--- build.rs
+++ build.rs
@@ -1,15 +1,18 @@
 fn main() {
     #[cfg(target_os = "macos")]
     {
+        let deployment_target = std::env::var("MACOSX_DEPLOYMENT_TARGET")
+            .unwrap_or_else(|_| "13.0".to_string());
+        println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET={}", deployment_target);
         // For macOS ARM (Apple Silicon)
         #[cfg(target_arch = "aarch64")]
         {
-            // println!("cargo:rustc-link-lib=framework=Accelerate");
+            println!("cargo:rustc-link-lib=framework=Accelerate");
         }
         // For macOS Intel
         #[cfg(target_arch = "x86_64")]
         {
-            // println!("cargo:rustc-link-lib=framework=Accelerate");
+            println!("cargo:rustc-link-lib=framework=Accelerate");
         }
     }
     #[cfg(not(target_os = "macos"))]
